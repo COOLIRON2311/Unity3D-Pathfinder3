670e2b4a5dd1fe2ea0934295562878d2eef6b262
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2024-04-08 14:59:22 +0300

    Adjusted parameters

--- Assets/Scripts/AI/Agent2Script.cs
+++ Assets/Scripts/AI/Agent2Script.cs
@@ -6,9 +6,17 @@ using UnityEngine.AI;
 public class Agent2Script : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Vector3? target;
+    public float UpdateInterval = 0.1f;
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SetDestination), 0.0f, 1.0f);
     }
 
     void Update()
@@ -19,7 +27,7 @@ public class Agent2Script : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var hit))
             {
-                agent.SetDestination(hit.point);
+                target = hit.point;
             }
         }
         if (Input.GetKeyDown(KeyCode.T))
@@ -27,8 +35,19 @@ public class Agent2Script : MonoBehaviour
             GameObject target = GameObject.FindGameObjectWithTag("Target");
             if (target != null)
             {
-                agent.SetDestination(target.transform.position);
+                Physics.Raycast(target.transform.position, Vector3.down * 100, out var hit);
+                this.target = hit.point;
             }
+        }
+    }
+
+    void SetDestination()
+    {
+        if (target.HasValue)
+        {
+            NavMeshPath path = new();
+            agent.CalculatePath(target.Value, path);
+            agent.SetPath(path);
         }
     }
 }
